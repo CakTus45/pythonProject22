8b798a8156c90960f02f7e0b1733f7f7a2874286
Alya <Aliyak2008@mail.ru>
2023-07-20 19:40:14 +0300

    новые файлы + изменение, 1

--- advertisments/app_advertisments/urls.py
+++ advertisments/app_advertisments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index_2, test
+from .views import index_2, test, test_2, top_sellers
 
 urlpatterns = [
-    path('', index_2),
-    path('test/', test)
+    path('', index_2, name='index'),
+    path('test/', test, name='test'),
+    path('test_2/',test_2, name='test_2'),
+    path('top_sellers', top_sellers,name='top_sellers')
 ]
--- advertisments/app_advertisments/views.py
+++ advertisments/app_advertisments/views.py
@@ -5,5 +5,11 @@ from django.http import HttpResponse
 def index_2(request_2):
     return render(request_2, "index.html")
 
+def top_sellers(request_2):
+    return render(request_2, "top-sellers.html")
+
 def test(request_2):
     return render(request_2, "test.html")
+
+def test_2(request_2):
+    return render(request_2, "test_2.html")
